834abd74bc2b80bf9eff3c8c38e9b4f57ffbaeb6
franco nicolas palma muÑoz <franco.palma@alumnos.inf.utfsm.cl>
2021-11-05 19:45:16 -0300

    grpc pozo-lider

---
+++ Pozo/grpc/1server.go
@@ -0,0 +1,37 @@
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	// "context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "github.com/Zes7one/dLab/Pozo/consulta"
+
+	// pb "google.golang.org/grpc/examples/helloworld/h"
+)
+
+const (
+	port = ":50051"
+)
+
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := consulta.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	consulta.RegisterConsultaServiceServer(grpcServer, &s)
+
+	// pb.RegisterGreeterServer(s, &server{})
+	// log.Printf("server listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ Pozo/grpc/consulta/consulta.go
@@ -0,0 +1,21 @@
+package consulta
+ 
+import(
+	"log"
+
+	"golang.org/x/net/context"
+)
+
+type Server struct{
+}
+
+func (s *Server) PeticionPozo(ctx context.Context, message *Message)(*Message, error){
+	log.Printf("Mensaje recibido desde cliente: %s ", message.Body)
+	return &Message{Body: "Hola desde el server" }, nil
+}
+
+
+// func (s *Server) PeticionPozo(ctx context.Context, message *Message, int pozoActual)(*Message, error){
+// 	log.Printf("Mensaje recibido desde cliente: %s ", message.Body)
+// 	return &Message{Body: pozoActual }, nil
+// }
